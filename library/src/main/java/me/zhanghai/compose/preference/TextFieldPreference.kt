c58fb29c01600365c5d4ce3642c7e4bf5a687ee0
Hai Zhang <dreaming.in.code.zh@gmail.com>
2024-09-10 13:05:02 -0700

    Refactor: Remove unused import

--- library/src/main/java/me/zhanghai/compose/preference/TextFieldPreference.kt
+++ library/src/main/java/me/zhanghai/compose/preference/TextFieldPreference.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Icon
