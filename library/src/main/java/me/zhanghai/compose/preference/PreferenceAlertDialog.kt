9b7de360945de99666d57cec2938da842fb599b8
Hai Zhang <dreaming.in.code.zh@gmail.com>
2024-09-10 12:43:23 -0700

    Fix: Fix alert dialog button vertical padding

--- library/src/main/java/me/zhanghai/compose/preference/PreferenceAlertDialog.kt
+++ library/src/main/java/me/zhanghai/compose/preference/PreferenceAlertDialog.kt
@@ -25,7 +25,7 @@ import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -85,7 +85,7 @@ internal fun PreferenceAlertDialog(
                     ) {
                         AlertDialogFlowRow(mainAxisSpacing = 8.dp, crossAxisSpacing = 12.dp) {
                             CompositionLocalProvider(
-                                LocalMinimumInteractiveComponentEnforcement provides false,
+                                LocalMinimumInteractiveComponentSize provides Dp.Unspecified,
                                 content = buttons,
                             )
                         }
