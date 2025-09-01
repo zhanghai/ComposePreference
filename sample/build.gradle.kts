3caf8d014d2282f0942ad75cd64c692d048934f0
Hai Zhang <dreaming.in.code.zh@gmail.com>
2025-09-01 12:15:59 -0700

    Build: Rename commonJs source set to web
    Following upcoming change in Kotlin 2.2.20.

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -71,12 +71,13 @@ kotlin {
         jvmMain { dependsOn(commonJvmMain) }
         jvmTest { dependsOn(commonJvmTest) }
 
-        val commonJsMain by creating { dependsOn(commonMain.get()) }
-        val commonJsTest by creating { dependsOn(commonTest.get()) }
-        jsMain { dependsOn(commonJsMain) }
-        jsTest { dependsOn(commonJsTest) }
-        wasmJsMain { dependsOn(commonJsMain) }
-        wasmJsTest { dependsOn(commonJsTest) }
+        // TODO: Migrate to official web source sets once upgraded to Kotlin 2.2.20.
+        val webMain by creating { dependsOn(commonMain.get()) }
+        val webTest by creating { dependsOn(commonTest.get()) }
+        jsMain { dependsOn(webMain) }
+        jsTest { dependsOn(webTest) }
+        wasmJsMain { dependsOn(webMain) }
+        wasmJsTest { dependsOn(webTest) }
 
         commonMain {
             dependencies {
